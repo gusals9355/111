af2e38056130ccdca01f23009af60c7ff0ed1487
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-03-31 10:33:13 +0900

    32

---
+++ chap01/src/ch04/DoubleForExam.java
@@ -0,0 +1,14 @@
+package ch04;
+
+public class DoubleForExam {
+
+	public static void main(String[] args) {
+		for(int i=0; i<3; i++) {
+			for(int j=100; j<105; j++) {
+				System.out.printf("%d - %d \n",i,j);
+			}
+		}
+		
+	}
+
+}
